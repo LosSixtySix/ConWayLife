f2c77044c7e7bfe79c8f53af36049048fdacc405
LosSixtySix <lossixtysix@gmail.com>
2025-05-13 19:08:25 -0600

    Setup Rules and implemented pause and start button

--- ui.js
+++ ui.js
@@ -4,11 +4,27 @@ const isKeyDown = (key) => pressedKeys.has(key)
 
 let positionsTooFill = [];
 
-let deadCells = new Set();
-
-let livingCells = new Set();
+const grid = Array.from({length: 120}, () => new Array(120).fill(0));
 
 const gameTime = 500;
+
+var start = false
+
+const startButton = document.getElementById("StartGame")
+
+const pauseButton = document.getElementById("PauseGame")
+
+const startGame = (event) =>{
+    event.preventDefault()
+    start = true
+}
+const pauseGame = (event)=>{
+    event.preventDefault()
+    start = false
+}
+
+startButton.addEventListener("click",startGame)
+pauseButton.addEventListener("click",pauseGame)
 
 var myGameArea = {
     canvas: document.createElement("canvas"),
@@ -24,15 +40,27 @@ var myGameArea = {
         this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
 
+
+
+
         document.body.appendChild(this.canvas)
+
+
+
+        
 
         this.canvas.onmousedown = (event) =>{
             if(event.button === 0)
             {
                 const mousePosition = this.getMousePos(event)
-                console.log(mousePosition)
-                
-                positionsTooFill.push(mousePosition)
+
+                const gridX = Math.trunc(mousePosition.x/5)
+                const gridY = Math.trunc(mousePosition.y/5)
+                if(grid[gridX][gridY] != 1)
+                {
+                    grid[gridX][gridY] = 1
+                }
+
             }
         }
 
@@ -41,19 +69,109 @@ var myGameArea = {
 
 const update = async() =>{
     requestAnimationFrame(()=>{
-
-        while(positionsTooFill.length > 0)
+        myGameArea.context.clearRect(0,0,myGameArea.canvas.width,myGameArea.canvas.height)
+        if(start)
         {
-            const Position = positionsTooFill.pop()
+            for(let x =0; x < grid.length -1; x ++)
+            {
+                for(let y = 0; y < grid[x].length-1; y ++)
+                {
+                    let liveNeighbors = 0
+                    if(x > 0)
+                    {
+                        if(grid[x-1][y] === 1)
+                        {
+                            liveNeighbors +=1
+                        }
+                        if(y > 0)
+                        {
+                            if(grid[x-1][y-1] === 1)
+                            {
+                                liveNeighbors += 1
+                            }
+                        }
+                        if( y < grid[x].length)
+                        {
+                            if(grid[x-1][y+1] === 1)
+                            {
+                                liveNeighbors += 1
+                            }
+                        }
+                        
+                    }
+                    if(y > 0)
+                    {
+                        if(x < grid[x].length)
+                        {
+                            if(grid[x+1][y-1] === 1)
+                            {
+                                liveNeighbors += 1
+                            }
+                        }
+                        if(grid[x][y-1] === 1)
+                            {
+                                liveNeighbors +=1
+                            }
+                    }
 
-            myGameArea.context.fillStyle = "#000000"
-            myGameArea.context.fillRect(Position.x,Position.y,4,4)
 
-            livingCells.add(Position)
+                    if(x < grid[x].length && y < grid[x].length)
+                    {
+                        if(grid[x+1][y] === 1)
+                        {
+                            liveNeighbors +=1
+                        }
+
+                        if(grid[x][y+1] === 1)
+                        {
+                            liveNeighbors +=1
+                        }
+                        if(grid[x+1][y+1] === 1)
+                        {
+                            liveNeighbors +=1
+                        }
+                    }
+
+
+                    if(liveNeighbors < 2)
+                    {
+                        grid[x][y] = 0
+                    }
+                    if(liveNeighbors > 3)
+                    {
+                        grid[x][y] = 0
+                    }
+                    if(liveNeighbors ===3)
+                    {
+                        grid[x][y] = 1
+                    }
+                    if(grid[x][y] === 1)
+                    {
+                        myGameArea.context.fillStyle = "#000000"
+                        myGameArea.context.fillRect(x*5,y*5,5,5)
+                    }
+                }
+            }
+        }
+        else
+        {
+            for(let x =0; x < grid.length -1; x ++)
+                {
+                    for(let y = 0; y < grid[x].length-1; y ++)
+                    {
+                        if(grid[x][y] === 1)
+                            {
+                                myGameArea.context.fillStyle = "#000000"
+                                myGameArea.context.fillRect(x*5,y*5,5,5)
+                            }
+                    }
+                }
+
         }
 
-
+        
         setTimeout(()=> update(),gameTime)
+        
     })
 }
 
