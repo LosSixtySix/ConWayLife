c0ca616d85c9ef12be811ee6fee2388fceacd80d
LosSixtySix <112432787+LosSixtySix@users.noreply.github.com>
2025-05-14 10:17:16 -0600

    Created cell class to contain color and position

--- ui.js
+++ ui.js
@@ -11,6 +11,15 @@ const gameTime = 25;
 
 const colors = ["#1b70c4","#9d7d01","#1b3e8d","#992c99","#539d84","#5d4f15","#5457de"]
 
+class Cell {
+    constructor(x,y,color){
+        this.x = x;
+        this.y = y
+        this.color = color;
+    }
+}
+
+
 var start = false
 
 const startButton = document.getElementById("StartGame")
@@ -59,9 +68,12 @@ var myGameArea = {
 
                 const gridX = Math.trunc(mousePosition.x/5)
                 const gridY = Math.trunc(mousePosition.y/5)
-                if(grid[gridX][gridY] != 1)
+                if(grid[gridX][gridY] === 0)
                 {
-                    grid[gridX][gridY] = 1
+                    let RandomNumber = Math.floor(Math.random() * colors.length)
+                    const newCell = new Cell(gridX,gridY,colors[RandomNumber])
+                    grid[gridX][gridY] = newCell
+                    console.log(grid[gridX][gridY])
                 }
 
             }
@@ -70,7 +82,6 @@ var myGameArea = {
     }
 }
 
-let RandomNumber = Math.floor(Math.random() * colors.length)
 
 const update = async() =>{
     requestAnimationFrame(()=>{
@@ -85,20 +96,20 @@ const update = async() =>{
                     let liveNeighbors = 0
                     if(x > 0)
                     {
-                        if(grid[x-1][y] === 1)
+                        if(grid[x-1][y] != 0)
                         {
                             liveNeighbors +=1
                         }
                         if(y > 0)
                         {
-                            if(grid[x-1][y-1] === 1)
+                            if(grid[x-1][y-1] != 0)
                             {
                                 liveNeighbors += 1
                             }
                         }
                         if( y < grid[x].length)
                         {
-                            if(grid[x-1][y+1] === 1)
+                            if(grid[x-1][y+1] != 0)
                             {
                                 liveNeighbors += 1
                             }
@@ -109,12 +120,12 @@ const update = async() =>{
                     {
                         if(x < grid[x].length)
                         {
-                            if(grid[x+1][y-1] === 1)
+                            if(grid[x+1][y-1] != 0)
                             {
                                 liveNeighbors += 1
                             }
                         }
-                        if(grid[x][y-1] === 1)
+                        if(grid[x][y-1] != 0)
                             {
                                 liveNeighbors +=1
                             }
@@ -123,16 +134,16 @@ const update = async() =>{
 
                     if(x < grid[x].length && y < grid[x].length)
                     {
-                        if(grid[x+1][y] === 1)
+                        if(grid[x+1][y] != 0)
                         {
                             liveNeighbors +=1
                         }
 
-                        if(grid[x][y+1] === 1)
+                        if(grid[x][y+1] != 0)
                         {
                             liveNeighbors +=1
                         }
-                        if(grid[x+1][y+1] === 1)
+                        if(grid[x+1][y+1] != 0)
                         {
                             liveNeighbors +=1
                         }
@@ -149,12 +160,16 @@ const update = async() =>{
                     }
                     if(liveNeighbors ===3)
                     {
-                        grid[x][y] = 1
+
+                        let RandomNumber = Math.floor(Math.random() * colors.length)
+                        const newCell = new Cell(x,y,colors[RandomNumber])
+                        grid[x][y] = newCell
                     }
-                    if(grid[x][y] === 1)
+                    if(grid[x][y] != 0)
                     {
-                        myGameArea.context.fillStyle = colors[RandomNumber]
-                        myGameArea.context.fillRect(x*5,y*5,5,5)
+                        const cell = grid[x][y]
+                        myGameArea.context.fillStyle = cell.color
+                        myGameArea.context.fillRect(cell.x*5,cell.y*5,5,5)
                     }
                 }
             }
@@ -165,10 +180,11 @@ const update = async() =>{
                 {
                     for(let y = 0; y < grid[x].length-1; y ++)
                     {
-                        if(grid[x][y] === 1)
+                        if(grid[x][y] != 0)
                             {
-                                myGameArea.context.fillStyle = colors[RandomNumber]
-                                myGameArea.context.fillRect(x*5,y*5,5,5)
+                                const cell = grid[x][y]
+                                myGameArea.context.fillStyle = cell.color
+                                myGameArea.context.fillRect(cell.x*5,cell.y*5,5,5)
                             }
                     }
                 }
